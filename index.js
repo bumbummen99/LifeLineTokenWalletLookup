d779f9d84b7096d41fe7d92c2a605fb430387ce8
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-19 01:16:54 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const bscscan = require('@skyraptor/bsc-scan').default;
+const token = require('@skyraptor/bsc-scan').token;
+
+/* Load the configuration */
+dotenv.config({
+    path: path.resolve(process.cwd() + '/.env'),
+});
+
+async function getWalletBalance(wallet) {
+    try {
+        return await token.getAccountBalanceForTokenContractAddress(wallet, '0xd37c1417da7bf5b02ffdea8d5427022dc88a0ee2', 6574878);
+        //return await token.getHistoricAccountBalanceForTokenContractAddress(wallet, '0xd37c1417da7bf5b02ffdea8d5427022dc88a0ee2', 6574878);
+        
+    } catch (err) {
+        console.log(err)
+    }
+
+    return null;
+}
+
+const run = async () => {
+    const wallet = '0xc097046b82fc26ae6a0058BaF0Fe9fE10b750384';
+
+    // https://bscscan.com/myapikey
+    bscscan.setApiKey(process.env.BSCSCAN_API_TOKEN)
+
+    const balance = await getWalletBalance(wallet);
+
+    console.log(`Account '${wallet}' has ${balance}LLT.`);
+}
+
+run();
